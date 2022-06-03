bbd7708d719379c2f54b2e8e1853a6aacb755791
Shreya_Kashyap <kashyapshreya13@gmail.com>
2022-06-03 22:07:47 +0530

    "data-user-cards-container added"

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const userCardTemplate = document.querySelector("[data-user-template")        
+const userCardTemplate = document.querySelector("[data-user-template")  
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+
         fetch("movies.json")
           .then(res => res.json())
           .then(data=>{
-            const card = userCardTemplate.content.cloneNode(true).children;
+            const card = userCardTemplate.textContent.cloneNode(true).children[0];
+            const header =card.querySelector("[data-heade]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = user.name
+            body.textContent = user.email
+            userCardContainer.append(card)
             console.log(card)
           })
         
